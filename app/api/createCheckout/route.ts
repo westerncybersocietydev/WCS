4a8557a64300cec75680161e4b80e38ce31a2f56
fabdull9 <fabdull9@uwo.ca>
2024-09-16 00:02:55 -0400

    fix

--- app/api/createCheckout/route.ts
+++ app/api/createCheckout/route.ts
@@ -1,27 +1,27 @@
-import { NextApiRequest, NextApiResponse } from 'next';
+import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: '2024-06-20',
 });
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    try {
-      const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: req.body.line_items,
-        mode: 'payment',
-        success_url: `${req.headers.origin}/success`,
-        cancel_url: `${req.headers.origin}/cancel`,
-      });
-      
-      res.status(200).json({ id: session.id });
-    } catch (error) {
-      res.status(500).json({ error: error });
-    }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: body.line_items,
+      mode: 'payment',
+      success_url: `${req.headers.get('origin')}/success`,
+      cancel_url: `${req.headers.get('origin')}/cancel`,
+    });
+    
+    return NextResponse.json({ id: session.id });
+  } catch (error) {
+    return NextResponse.json({ error: error }, { status: 500 });
   }
+}
+
+export async function GET() {
+  return NextResponse.json({ message: 'Method Not Allowed' }, { status: 405 });
 }
