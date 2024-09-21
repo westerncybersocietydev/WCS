2c08113670b9bbea9ee902eca49d9f2ab90ff0e6
fabdull9 <fahmidabdullah@hotmail.com>
2024-09-21 15:49:52 -0400

    fix

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -4,7 +4,6 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useCallback, useState, useRef, useEffect } from 'react';
 import { useUser } from '../context/UserContext'; // Adjust the path as 
-import Avatar from '../dataFiles/avatars';
 import { getProfile, updatePlan } from '../lib/actions/user.action';
 import BecomeVIP from './becomeVIP';
 import toast from 'react-hot-toast';
