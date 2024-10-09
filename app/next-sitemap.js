0959befdb063b7ce4a67e2fcf15d04fc9dffed38
fabdull9 <fahmidabdullah@hotmail.com>
2024-10-09 12:49:22 -0400

    site-map and robots.txt

---
+++ app/next-sitemap.js
@@ -0,0 +1,13 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+    siteUrl: 'https://www.westerncybersociety.ca',
+    generateRobotsTxt: true,
+    exclude: ['/sign-in', '/sign-up', '/myevents', '/profile'], // pages to exclude
+    robotsTxtOptions: {
+        additionalSitemaps: [
+            'https://www.westerncybersociety.ca/sitemap.xml',
+        ],
+    },
+};
+
+module.exports = config;
